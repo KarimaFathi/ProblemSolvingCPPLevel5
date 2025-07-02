24227bf66144a1841a551301a3352c1302284b14
KarimaFathi <karimafathifadili@gmail.com>
2025-07-02 19:17:30 +0100

    Project 2: Implement more functionality in the Queue class

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -23,5 +23,32 @@ int main()
 	cout << "Queue's front : " << myQueue.front() << endl;
 	cout << "Queue's back  : " << myQueue.back() << endl;
 
+	cout << "Item in pos 2 : " << myQueue.getItem(2);
+
+	myQueue.reverse();
+	cout << "\nQueue after reverse : ";
+	myQueue.print();
+
+	myQueue.insertAfter(2, 800);
+	cout << "\nQueue after inserting 800 after item at index 2 : ";
+	myQueue.print();
+
+
+	myQueue.updateItem(1, 200);
+	cout << "\nupdate item at index 2 : ";
+	myQueue.print();
+
+	myQueue.insertAtFront(500);
+	cout << "\nQueue after inserting 500 at front : ";
+	myQueue.print();
+
+	myQueue.insertAtBack(900);
+	cout << "\nQueue after inserting 900 at back : ";
+	myQueue.print();
+
+
+	myQueue.clear();
+	cout << "\nQueue after clear : ";
+	myQueue.print();
 }
 
--- Project1/clsMyQueue.h
+++ Project1/clsMyQueue.h
@@ -36,5 +36,37 @@ public:
 	T back() {
 		return _myList.getItem(size() - 1);
 	}
+	//1
+	T getItem(int index) {
+		return _myList.getItem(index);
+	}
+	//2
+	void reverse() {
+		_myList.reverse();
+	}
+	//3
+	void updateItem(int index, T val) {
+		_myList.updateItem(index, val);
+	}
+	//4
+	void insertAfter(int index, T val) {
+		_myList.insertAfter(index, val);
+	}
+	//5
+	void insertAtFront(T val) {
+		_myList.insertAtBeginning(val);
+	}
+	//6
+	void insertAtBack(T val) {
+		_myList.insertAtEnd(val);
+	}
+
+	//7 
+	void clear() {
+		_myList.clear();
+	}
+
+
+
 };
 
